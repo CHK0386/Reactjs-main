742a62efed81def8760af18661184cde321ca0ce
Khang Cao Hoàng <tinlun0386@gmail.com>
2022-10-21 12:06:46 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-// import Heading from './components/Heading';
+import Login from './components/Heading/Login.js';
 
 function App() {
+    
     return(
-        <div>
-            <h1>Hello</h1>
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/RgKAFK5djSk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-            
-            {/* <Heading/> */}
+        <div className="App">
+            <Login />
         </div>
-
     );
 }
 export default App;
+
--- src/components/Heading/index.js
+++
@@ -1,9 +0,0 @@
-// import './Heading.css'
-
-function Heading() {
-    return(
-        <h1 className="heading">Hello</h1>
-    )
-}
-
-export default Heading;
