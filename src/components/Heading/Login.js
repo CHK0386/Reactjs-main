47dc3b02797d7c33f22fb23c1aba0c8935d5faf4
Khang Cao Hoàng <tinlun0386@gmail.com>
2022-10-26 21:41:08 +0700

    add UI

--- src/components/Heading/Login.js
+++ src/components/Heading/Login.js
@@ -1,83 +1,33 @@
 import React from "react";
 import logo from "../Asset/logo.jpg";
 import "./Heading.css";
+// import {useState} from 'react'
 
 function Login() {
+  
     return (
-      <section className="vh-100">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-sm-6 text-black">
-              <div className="px-5 ms-xl-4">
-                <img src={logo} alt={logo} className="logo-icon" />
-                {/* <div className="h1 fw-bold mb-0">Appointment Booking System</div> */}
-              </div>
-  
-              <div className="d-flex align-items-center h-custom-2 px-5 ms-xl-4 mt-5 pt-5 pt-xl-0 mt-xl-n5">
-                <form style={{ width: "23rem" }}>
-                  <h3
-                    className="fw-normal mb-3 pb-3"
-                    style={{ letterSpacing: "1px", textAlign: "center" }}
-                  >
-                    LOGIN
-                  </h3>
-  
-                  <div className="form-outline mb-4">
-                    <input
-                      type="email"
-                      // id="form2Example18"
-                      className="form-control form-control-lg"
-                    />
-                    <label className="form-label" htmlFor="form2Example18">
-                      Email address
-                    </label>
-                  </div>
-  
-                  <div className="form-outline mb-4">
-                    <input
-                      type="password"
-                      // id="form2Example28"
-                      className="form-control form-control-lg"
-                    />
-                    <label className="form-label" htmlFor="form2Example28">
-                      Password
-                    </label>
-                  </div>
-  
-                  <div className="pt-1 mb-4">
-                    <button
-                      className="btn btn-info btn-lg btn-block"
-                      type="button"
-                    >
-                      Login
-                    </button>
-                  </div>
-  
-                  <p className="small mb-5 pb-lg-2">
-                    <a className="text-muted" href="#!">
-                      Forgot password?
-                    </a>
-                  </p>
-                  <p>
-                    Don't have an account?{" "}
-                    <a href="#!" className="link-info">
-                      Register here
-                    </a>
-                  </p>
-                </form>
-              </div>
-            </div>
-            {/* <div className="col-sm-6 px-0 d-none d-sm-block">
-              <img
-                src={"https://source.unsplash.com/random?clinic"}
-                alt={"logo"}
-                className="w-100 vh-100"
-                style={{ objectFit: "cover", objectPosition: "left" }}
-              />
-            </div> */}
-          </div>
-        </div>
-      </section>
+      <div class="login-box">
+          <img src={logo} alt={logo} className="logo-icon" />
+              {/* <div className="h1 fw-bold mb-0">Appointment Booking System</div> */}
+          <h2>Login</h2>
+  <form>
+    <div class="user-box">
+      <input type="text" name="" required=""/>
+      <label>Username</label>
+    </div>
+    <div class="user-box">
+      <input type="password" name="" required=""/>
+      <label>Password</label>
+    </div>
+    <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Submit
+    </a>
+  </form>
+</div>
     );
   }
   
