521052827152dd059f33f1931b2786ba0fdb6821
Khang Cao Hoàng <tinlun0386@gmail.com>
2022-10-29 14:25:11 +0700

    add

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import { useEffect, useState } from 'react';
-import productApi from './api/productApi.js';
+// import { useEffect, useState } from 'react';
+// import productApi from './api/productApi.js';
 import Login from './components/Heading/Login.js';
 
 function App() {
-    const [productlist, setProductList] = useState([]);
+    // const [productlist, setProductList] = useState([]);
 
-    useEffect(() => {
-        const fetchProductlist = async () => {
-            try {
-                const response = await productApi.getAll();
-                console.log(response);
-            } catch (error) {
-                console.log('Failed to fetch product list: ', error);
-            }
-            fetchProductlist();
-        }
-    },[]);
+    // useEffect(() => {
+    //     const fetchProductlist = async () => {
+    //         try {
+    //             const response = await productApi.getAll();
+    //             console.log(response);
+    //         } catch (error) {
+    //             console.log('Failed to fetch product list: ', error);
+    //         }
+    //         fetchProductlist();
+    //     }
+    // },[]);
     
     return(
         <div className="App">
