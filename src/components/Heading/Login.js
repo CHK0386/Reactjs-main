5bd48edcc0c93afe3729ab4b37acac1feb7f4f3c
Khang Cao Hoàng <tinlun0386@gmail.com>
2022-11-11 13:55:25 +0700

    update

--- src/components/Heading/Login.js
+++ src/components/Heading/Login.js
@@ -1,47 +1,20 @@
-// import React, { useEffect, useState } from "react";
-// import logo from "../Asset/logo.jpg";
 import "./Heading.css";
 import "./API.js";
 
-// import {useState} from 'react'
-// import {useHistory} from 'react-route-dom'
-
-
 function Login() {
-  // const [email, setEmail] =useState("");
-  // const [password, setPassword] =useState("");
-  // const history = useHistory()
-  // useEffect(()=> {
-  //   if (localStorage.getItem('user-info')){
-  //     history.push("/add")
-  //   }
-  // },[])
-  // function login() {
-  //   console.warn(email,password)
-  //   let item={email,password}
-  //   let result= fetch("http://localhost:3000/posts",{
-  //     method:'POST',
-  //     headers:{
-  //       "Content-Type":"application/json",
-  //       "Accept":'application/json'
-  //     },
-  //     body: JSON.stringify(item)
-
-  //     });
-  // }
   
   return (
     <div class="main">
 
       <form action="" method="POST" class="form" id="form-1">
         <h3 class="heading">Thành viên đăng ký</h3>
-        <p class="desc">Cùng nhau học lập trình miễn phí tại F8 ❤️</p>
+        {/* <p class="desc">Cùng nhau học lập trình miễn phí tại F8 ❤️</p> */}
 
         <div class="spacer"></div>
 
         <div class="form-group">
           <label for="fullname" class="form-label">Tên đầy đủ</label>
-          <input id="fullname" name="fullname" type="text" placeholder="VD: Sơn Đặng" class="form-control"/>
+          <input id="fullname" name="fullname" type="text" placeholder="VD: Hoang Khang" class="form-control"/>
           <span class="form-message"></span>
         </div>
 
@@ -68,7 +41,7 @@ function Login() {
 
       <form action="" method="POST" class="form" id="form-2">
         <h3 class="heading">Đăng nhập</h3>
-        <p class="desc">Cùng nhau học lập trình miễn phí tại F8 ❤️</p>
+        {/* <p class="desc">Cùng nhau học lập trình miễn phí tại F8 ❤️</p> */}
 
         <div class="spacer"></div>
 
